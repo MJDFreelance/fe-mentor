5e2b8b8651470479f457bba1fbac6eb427e6c3c6
MichaelJDFreelance <60489684+MichaelJDFreelance@users.noreply.github.com>
2025-01-30 20:15:41 +0000

    Add alt attribute to img

--- src/app/developer-profile/saveMessage.ts
+++ src/app/developer-profile/saveMessage.ts
@@ -1,19 +1,33 @@
 "use server";
 
-import { PutCommand } from "@aws-sdk/lib-dynamodb";
-import getDynamoClient from "@/lib/dynamoClient";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { fromWebToken } from "@aws-sdk/credential-provider-web-identity";
 
 export const saveMessage = async (contact: any) => {
   try {
     // Dynamically fetch the client
-    const dynamoClient = getDynamoClient();
+    console.error("No documentClient found");
+    console.log(process.env.AWS_REGION);
+    console.log(process.env.AWS_ROLE_ARN);
+    console.log(process.env.VERCEL_OIDC_TOKEN);
+
+    const dynamoClient = new DynamoDBClient({
+      region: process.env.AWS_REGION || "us-east-1", // Use region from .env, fallback to "us-east-1"
+      credentials: fromWebToken({
+        roleArn: process.env.AWS_ROLE_ARN!, // Load role ARN from the .env file
+        webIdentityToken: process.env.VERCEL_OIDC_TOKEN!,
+      }),
+    });
+
+    const documentClient = DynamoDBDocumentClient.from(dynamoClient);
 
     const command = new PutCommand({
       TableName: "fe_contacts",
       Item: contact,
     });
 
-    const result = await dynamoClient.send(command);
+    const result = await documentClient.send(command);
 
     return "done";
   } catch (error: any) {
